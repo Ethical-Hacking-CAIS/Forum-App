744d09ffe1c45affeb3523307a04810d0da191a2
DiegoSalas27 <dominicsc2hs@gmail.com>
2022-09-17 18:02:35 -0500

    feat: addtemplate for reading posts

--- routes/index.ts
+++ routes/index.ts
@@ -1,8 +1,12 @@
 import express from 'express'
+import formatDate from '../utils/formatDate'
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Blog app'})
+  let pageInfo: any = {}
+  pageInfo.title =  'Blog app',
+  pageInfo.formatDate = formatDate
+  res.render('posts', pageInfo)
 })
 
 export default router
--- server.ts
+++ server.ts
@@ -5,11 +5,12 @@ var path = require('path');
 import express from 'express'
 
 import indexRouter from './routes/index'
+// import aboutRouter from './routes/about'
 
 const app = express()
 
 app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'jade')
+app.set('view engine', 'pug')
 
 app.use(logger('dev'))
 app.use(express.json())
@@ -17,6 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+// app.use('/about', aboutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
