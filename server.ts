5bef5a052b021a58c3980396cb353c99cd55ab6f
DiegoSalas27 <dominicsc2hs@gmail.com>
2022-09-17 10:02:34 -0500

    feat: add template engine

--- server.ts
+++ server.ts
@@ -1,12 +1,37 @@
+import { NextFunction, Request, Response } from 'express';
+import createError from 'http-errors'
+import logger from 'morgan'
+var path = require('path');
 import express from 'express'
+
+import indexRouter from './routes/index'
+
 const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Yellow')
-})
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
-})
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err: any, req: Request, res: Response, next: NextFunction) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
